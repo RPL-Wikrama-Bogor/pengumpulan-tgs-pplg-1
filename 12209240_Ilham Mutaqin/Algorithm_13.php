4372dfc4bbef67f36ac36280e563153a07205d4c
Ilhammtqn/CV-Ilham <muhammadIlhamMutaqin@smkwikrama.sch.id>
2023-08-31 11:07:51 +0700

    File 14

--- 12209240_Ilham Mutaqin/Algorithm_13.php
+++ 12209240_Ilham Mutaqin/Algorithm_13.php
@@ -1,19 +1,9 @@
-<?PHP
+<?php 
 
-$i=0;
+$i = 1;
 
+for($i = 1; $i <=50; $i++){
+    echo "$i <br>";
+}
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
