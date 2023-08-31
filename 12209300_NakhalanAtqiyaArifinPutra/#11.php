25c387991d466fce7f1d662b7aad94757f6621d6
Orangejahad <nakhalanatqiya@gmail.com>
2023-08-31 09:42:15 +0700

    12209300_NakhalanAtqiyaArifinPutra

---
+++ 12209300_NakhalanAtqiyaArifinPutra/#11.php
@@ -0,0 +1,18 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pengulangan</title>
+</head>
+<body>
+    <?php
+$i=0;
+for($i; $i <= 50; $i++){
+    echo $i."<br>";
+}
+?>
+</body>
+</html>
---
+++ 12209300_NakhalanAtqiyaArifinPutra/#13.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bilangan Genap</title>
+</head>
+<body>
+    <?php
+    
+    $i=0;
+    for($i;$i<=50;$i++){
+        if($i%2==0){
+            echo"Ini bilangan Genap: $i <br>";
+        }
+    }
+    ?>
+</body>
+</html>
