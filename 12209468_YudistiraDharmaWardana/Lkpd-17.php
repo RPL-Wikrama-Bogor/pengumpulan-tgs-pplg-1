8295f8b74b1a972849e8dc666eea4058ec0a9833
dwyudistira <yudistiradharmawardana@smkwikrama.sch.id>
2023-09-01 08:57:11 +0700

    12209468_YudistiraDharmaWardana

--- 12209468_YudistiraDharmaWardana/Lkpd-17.php
+++ 12209468_YudistiraDharmaWardana/Lkpd-17.php
@@ -1,12 +1,3 @@
-<?php
-
-$i; 
-$terbesar; 
-$terkecil;
-$r_r;
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,16 +7,41 @@ $r_r;
 </head>
 <body>
 
-<form action="">
+<h1>Lkpd Algoritma 17</h1>
+    <form action="" method="post">
+        <?php for ($i = 1; $i <= 20; $i++) : ?>
+            Bilangan <?= $i ?>: <input type="number" name="number<?= $i ?>"><br>
+            <?php endfor; ?>
+            <input type="submit" name="submit" value="Hitung">
 
 
-?>
-<input type="text">
 
+    </form>
 
+    <?php if(isset($_POST['submit'])) {
 
+        $numbers = [];
+        for($i = 1; $i <= 20; $i++) {
+            if(isset($_POST["numbers$i"])) {
+                $numbers = floatval($_POST["number$i"]);
+                array_push($numbers, $number);
+            }
+        }
+        if(!empety($numbers)) {
+            $count = count($numbers);
+            $max = max($numbers);
+            $min = min($numbers);
+            $sum = array_sum($numbers);
+            $average = $sum / $count;
 
-</form>
-
+            echo "Bilangan Terbesar: $max <br>";
+            echo "Bilangan Terkecil: $min <br>";
+            echo "Rata-Rata Bilangan: $average <br>";
+        }else {
+            echo "Tidak ada Bilangan Yang Dimasukan";
+        }
+    }
+    
+    ?>
 </body>
 </html>
