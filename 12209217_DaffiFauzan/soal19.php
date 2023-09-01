4c475feab31677c91ad0469652aa8c230ed530a4
DaffyFauzan <daffi.fauzan@gmail.com>
2023-09-01 14:59:56 +0700

    update/soal19

--- 12209217_DaffiFauzan/soal19.php
+++ 12209217_DaffiFauzan/soal19.php
@@ -7,10 +7,55 @@
 </head>
 <body>
     <form action="" method="post">
-        <label>Vip<input type="number" name= "vip" value="Vip"></label>
-        <label>Eksekutif<input type="number" name= "eksekutif" value="Eksekutif"></label>
-        <label>Ekonomi<input type="number" name= "ekonomi" value="ekonomi"></label>
-        <button type="submit" name="submit">Submit</button>
+        <div class="container">
+        <h1>Menghitung ticket</h1>
+        <label>Vip<input type="number" name= "vip" value="Vip" max="50"></label>
+        <label>Eksekutif<input type="number" name= "eksekutif" value="Eksekutif" max="50"></label>
+        <label>Ekonomi<input type="number" name= "ekonomi" value="ekonomi" max="50"></label>
+        <button type="submit" name="submit">Submit</button>  
+        </div>
     </form>
+    <?php
+    if (isset($_POST['submit'])) {
+    $jumlahTiketVIP = $_POST['vip'];
+    $jumlahTiketEksekutif = $_POST['eksekutif'];
+    $jumlahTiketEkonomi = $_POST['ekonomi'];
+
+    $hargaVIP = 100000;
+    $hargaEksekutif = 75000;
+    $hargaEkonomi = 50000;
+
+
+    if ($jumlahTiketVIP >= 35) {
+        $keuntunganVIP = $jumlahTiketVIP * $hargaVIP * 0.25;
+    } elseif ($jumlahTiketVIP >= 20) {
+        $keuntunganVIP = $jumlahTiketVIP * $hargaVIP * 0.15;
+    } else {
+        $keuntunganVIP = $jumlahTiketVIP * $hargaVIP * 0.05;
+    }
+
+    if ($jumlahTiketEksekutif >= 40) {
+        $keuntunganEksekutif = $jumlahTiketEksekutif * $hargaEksekutif * 0.20;
+    } elseif ($jumlahTiketEksekutif >= 20) {
+        $keuntunganEksekutif = $jumlahTiketEksekutif * $hargaEksekutif * 0.10;
+    } else {
+        $keuntunganEksekutif = $jumlahTiketEksekutif * $hargaEksekutif * 0.02;
+    }
+    $keuntunganEkonomi = $jumlahTiketEkonomi * $hargaEkonomi * 0.07;
+
+
+    $totalTiket = $jumlahTiketVIP + $jumlahTiketEksekutif + $jumlahTiketEkonomi;
+    $totalKeuntungan = $keuntunganVIP + $keuntunganEksekutif + $keuntunganEkonomi;
+    
+    echo "Keuntungan Tiket VIP: Rp " . number_format($keuntunganVIP) . "<br>";
+    echo "Keuntungan Tiket Eksekutif: Rp " . number_format($keuntunganEksekutif) . "<br>";
+    echo "Keuntungan Tiket Ekonomi: Rp " . number_format($keuntunganEkonomi) . "<br>";
+    echo "Jumlah Tiket VIP: " . $jumlahTiketVIP . "<br>";
+    echo "Jumlah Tiket Eksekutif: " . $jumlahTiketEksekutif . "<br>";
+    echo "Jumlah Tiket Ekonomi: " . $jumlahTiketEkonomi. "<br>";
+    echo "Jumlah Keuntungan keseluruhan: Rp ". number_format($totalKeuntungan). "<br>";
+    }
+?>
+
 </body>
 </html>
