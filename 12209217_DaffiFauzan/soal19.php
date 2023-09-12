0494e1e26c1861d0490a5811a2e91f9d2a4c1d3e
DaffyFauzan <daffi.fauzan@gmail.com>
2023-09-12 09:57:26 +0700

    Soal 19 css

--- 12209217_DaffiFauzan/soal19.php
+++ 12209217_DaffiFauzan/soal19.php
@@ -4,16 +4,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tiket VIP</title>
+    <Style>
+        .container{
+            display: flex;
+            flex-direction: column;
+            margin: 10px auto;
+        }
+        .form{
+            text-align: center;
+            border-radius: 30px;
+            background-color: #DAC0A3;
+        }
+        .form label{
+            font-size: 20px;
+        }
+        .form input{
+            width: 15vw;
+            height: 5vh;
+            border-radius: 10px;
+        }
+        .form button{
+            background-color: white;
+            font-size: medium;
+            margin: 50px;
+            width: 20vw;
+            height: 10vh;
+            border-radius: 20px;
+        }
+        .hasil{
+            text-align: center;
+            margin: 10px;
+            padding: 30px;
+            background-color: #DAC0A3;
+        }
+
+    </Style>
+    
 </head>
 <body>
     <form action="" method="post">
         <div class="container">
-        <h1>Menghitung ticket</h1>
-        <label>Vip<input type="number" name= "vip" value="Vip" max="50"></label>
-        <label>Eksekutif<input type="number" name= "eksekutif" value="Eksekutif" max="50"></label>
-        <label>Ekonomi<input type="number" name= "ekonomi" value="ekonomi" max="50"></label>
-        <button type="submit" name="submit">Submit</button>  
+        <div class="form">
+            <h1>Menghitung ticket</h1>
+        <label>Vip</label><br>
+        <input type="number" name= "vip" value="Vip" max="50" ><br>
+        <label>Eksekutif</label><br>
+            <input type="number" name= "eksekutif" value="Eksekutif" max="50"><br>
+        <label>Ekonomi</label><br>
+            <input type="number" name= "ekonomi" value="ekonomi" max="50"><br>
+        <button type="submit" name="submit">Submit</button> 
         </div>
+        
     </form>
     <?php
     if (isset($_POST['submit'])) {
@@ -46,7 +87,7 @@
 
     $totalTiket = $jumlahTiketVIP + $jumlahTiketEksekutif + $jumlahTiketEkonomi;
     $totalKeuntungan = $keuntunganVIP + $keuntunganEksekutif + $keuntunganEkonomi;
-    
+    echo '<div class="hasil">';
     echo "Keuntungan Tiket VIP: Rp " . number_format($keuntunganVIP) . "<br>";
     echo "Keuntungan Tiket Eksekutif: Rp " . number_format($keuntunganEksekutif) . "<br>";
     echo "Keuntungan Tiket Ekonomi: Rp " . number_format($keuntunganEkonomi) . "<br>";
@@ -54,8 +95,13 @@
     echo "Jumlah Tiket Eksekutif: " . $jumlahTiketEksekutif . "<br>";
     echo "Jumlah Tiket Ekonomi: " . $jumlahTiketEkonomi. "<br>";
     echo "Jumlah Keuntungan keseluruhan: Rp ". number_format($totalKeuntungan). "<br>";
+    echo '</div>';
     }
 ?>
 
+
+
+
+</div>
 </body>
 </html>
