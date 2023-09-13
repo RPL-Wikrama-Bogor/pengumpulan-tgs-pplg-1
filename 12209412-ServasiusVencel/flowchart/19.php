17a927bd1a5998517487fba1950f4353d2e519a2
vencel <vencelservasius@gmail.com>
2023-09-14 06:40:27 +0700

    folder flowchart

---
+++ 12209412-ServasiusVencel/flowchart/19.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Penghitungan Keuntungan Bioskop</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="card">
+    <h1>Lkpd Algoritma 19</h1>
+    <form method="post" action="">
+        <label for=""><b>Tiket VIP</b></label>
+        <input type="number" name="vip" placeholder="Tiket VIP Terjual" required>
+        <br>
+        <label for=""><b>Tiket Eksekutif</b></label>
+        <input type="number" name="eksekutif" placeholder="Jumlah Tiket Ekskutif Terjual" required>
+        <br>
+        <label for=""><b>Tiket Ekonomi</b></label>
+        <input type="number" name="ekonomi" placeholder="Jumlah Ekonomi Terjual" required>
+        <br>
+        
+        <button type="submit" name="submit" value="Hitung Keuntungan">Submit</button>
+    </form>
+</div>
+    
+
+    <?php
+    if(isset($_POST['submit'])) {
+        $vipSold = $_POST['vip'];
+        $eksekutifSold = $_POST['eksekutif'];
+        $ekonomiSold = $_POST['ekonomi'];
+        
+        $vipProfit = 0;
+        $eksekutifProfit = 0;
+        $ekonomiProfit = 0;
+        
+        if ($vipSold >= 35) {
+            $vipProfit = $vipSold * 50 * 0.25;
+        } elseif ($vipSold >= 20 && $vipSold < 35) {
+            $vipProfit = $vipSold * 50 * 0.15;
+        } else {
+            $vipProfit = $vipSold * 50 * 0.05;
+        }
+        
+        if ($eksekutifSold >= 40) {
+            $eksekutifProfit = $eksekutifSold * 50 * 0.20;
+        } elseif ($eksekutifSold >= 20 && $eksekutifSold < 40) {
+            $eksekutifProfit = $eksekutifSold * 50 * 0.10;
+        } else {
+            $eksekutifProfit = $eksekutifSold * 50 * 0.02;
+        }
+        
+        $ekonomiProfit = $ekonomiSold * 50 * 0.07;
+        
+        $totalProfit = $vipProfit + $eksekutifProfit + $ekonomiProfit;
+        $totalTickets = ($vipSold + $eksekutifSold + $ekonomiSold) * 50;
+
+        echo "<h3>Keuntungan Per Kelas:</h3>";
+        echo "VIP: $vipProfit<br>";
+        echo "Eksekutif: $eksekutifProfit<br>";
+        echo "Ekonomi: $ekonomiProfit<br><br>";
+        
+        echo "Keuntungan Keseluruhan: $totalProfit<br>";
+        echo "Jumlah Tiket VIP Terjual: " . ($vipSold * 50) . "<br>";
+        echo "Jumlah Tiket Eksekutif Terjual: " . ($eksekutifSold * 50) . "<br>";
+        echo "Jumlah Tiket Ekonomi Terjual: " . ($ekonomiSold * 50) . "<br>";
+        echo "Total Tiket Terjual: $totalTickets";
+    }
+    ?>
+</body>
+</html>
