ab2dbb6b13df1c6d37a52466129d30eee29f93e0
Orangejahad <nakhalanatqiya@gmail.com>
2023-09-15 12:57:05 +0700

    12209300

--- 12209300_NakhalanAtqiyaArifinPutra/#15.php
+++ 12209300_NakhalanAtqiyaArifinPutra/#15.php
@@ -1,49 +1,59 @@
-<?php
-
-$i= [
-    [, , ],
-    [, , ],
-    [, , ],
-
-];
-$terbesar;
-$terkecil;
-$rata_rata;
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nilai</title>
+    <title>Pencarian Juara Kelas</title>
 </head>
 <body>
-<h2>Nilai</h2>
+    <h2>Pencarian Juara Kelas</h2>
+    <form action="" method="post">
+        <?php
+        $mataPelajaran = array("MTK", "INDO", "INGG", "DPK", "Agama");
+        $siswaCount = 15;
+        
+        for ($i = 1; $i <= $siswaCount; $i++) {
+            echo "<h3>Siswa ke-$i</h3>";
+            echo "Nama: <input type='text' name='nama[]' required><br>";
 
-<form method="post" action="#">
-    <table>
-        <tr>
-            <td>Masukan Data</td>
-            <td>  :  </td>
-            <td><input type="number" name="dta"></td>
-        </tr>
-        <tr>
-            <td><input type="submit" name="submit" value="kirim"></td>
-        </tr>
-    </table>
-</form>
+            foreach ($mataPelajaran as $mapel) {
+                echo "$mapel: <input type='number' name='nilai[$i][$mapel]' required><br>";
+            }
 
-
-<?php
-        if (isset($_POST['submit'])) {
-            $i = $_POST ['dta'];
-          
-            echo $i;
-
-
+            echo "Kehadiran (persentase): <input type='number' name='kehadiran[$i]' required><br>";
+            echo "<br>";
         }
         ?>
+        <input type="submit" name="submit" value="Cari Juara">
+    </form>
+
+    <?php
+    if (isset($_POST['submit'])) {
+        $nilai = $_POST['nilai'];
+        $kehadiran = $_POST['kehadiran'];
+
+        $totalSiswa = count($nilai);
+        $juaraKelas = array();
+        
+        foreach ($nilai as $siswa => $mapelNilai) {
+            $nilaiTotal = array_sum($mapelNilai);
+            $nilaiRataRata = $nilaiTotal / count($mapelNilai);
+            
+            if ($nilaiTotal >= 475 && $kehadiran[$siswa] == 100) {
+                $juaraKelas[$siswa] = $nilaiRataRata;
+            }
+        }
+
+        if (empty($juaraKelas)) {
+            echo "<h3>Tidak ada juara kelas yang memenuhi kriteria</h3>";
+        } else {
+            arsort($juaraKelas);
+            $juara = key($juaraKelas);
+
+            echo "<h3>Juara Kelas</h3>";
+            echo "Nama: " . $_POST['nama'][$juara] . "<br>";
+            echo "Nilai Rata-rata: " . $juaraKelas[$juara] . "<br>";
+        }
+    }
+    ?>
 </body>
 </html>
+``
--- 12209300_NakhalanAtqiyaArifinPutra/#16.php
+++ 12209300_NakhalanAtqiyaArifinPutra/#16.php
@@ -1,95 +1,139 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nilai</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body> <h2>Hitung Nilai</h2>
+
+<body>
+
     <form action="" method="post">
-    <table>
+        <p>VIP (MAX = 50)</p>
+        <input type="number" name="vip">
+        <p>EKSEKUTIF (MAX = 50)</p>
+        <input type="number" name="eksekutif">
+        <p>EKONOMI (MAX = 50)</p>
+        <input type="number" name="ekonomi">
+        <button type="submit" name="submit"> Submit</button>
+        <br>
 
-    <h2>Kode Pegawai</h2>
+        <?php
+        if (isset($_POST['submit'])) {
+            $vip;
+            $eksekutif;
+            $ekonomi;
+            $jumlah_tiket = 50;
+            $keuntungan_vip;
+            $keuntungan_eksekutif;
+            $vip = $_POST['vip'];
+            $eksekutif = $_POST['eksekutif'];
+            $ekonomi = $_POST['ekonomi'];
 
-<form method="post" action="#">
-    <table>
-        <tr>
-            <td>Nama siswa</td>
-            <td>  :  </td>
-            <td><input type="number" name="sw"></td>
-        </tr>
-        <tr>
-            <td>Data kehadiran</td>
-            <td>  :  </td>
-            <td><input type="number" name="kh"></td>
-        </tr>
-        <tr>
-            <td>Nilai Bahasa Indonesia</td>
-            <td>  :  </td>
-            <td><input type="number" name="in"></td>
-        </tr>
-        <tr>
-            <td>Nilau Bahasa Inggris</td>
-            <td>  :  </td>
-            <td><input type="number" name="ing"></td>
-        </tr>
-        <tr>
-            <td>Nilai Matematika</td>
-            <td>  :  </td>
-            <td><input type="number" name="mt"></td>
-        </tr>
-        <tr>
-            <td>Nilai DPK</td>
-            <td>  :  </td>
-            <td><input type="number" name="dp"></td>
-        </tr>
-        <tr>
-            <td>Nilai Agama</td>
-            <td>  :  </td>
-            <td><input type="number" name="agm"></td>
-        </tr>
-        <tr>
-            <td><input type="submit" name="submit" value="kirim"></td>
-        </tr>
-    </table>
-</form>
+            if ($vip <= $jumlah_tiket) {
+                if ($eksekutif <= $jumlah_tiket) {
+                    if ($ekonomi <= $jumlah_tiket) {
 
+                        if ($vip >= 35) {
+                            $keuntungan_vip = 25;
+                            $keuntungan_ekonomi = 7;
+                            if ($eksekutif >= 40) {
+                                $keuntungan_eksekutif = 20;
+                                $total = $keuntungan_eksekutif + $keuntungan_ekonomi + $keuntungan_vip;
+                                $total_tiket = $vip + $eksekutif + $ekonomi;
+                                echo "Keuntungan VIP = " . $keuntungan_vip . "%" . "<br>" . "Keuntungan Eksekutif = " . $keuntungan_eksekutif . "%" . "<br>"
+                                    . "Keuntungan Ekonomi = " . $keuntungan_ekonomi . "%" . "<br>" . "Keuntungan Kesuluran = " . $total . "%" . "<br>" .
+                                    "Jumlah Tiket VIP = " . $vip . "<br>" . "Jumlah Tiket Eksekutif = " . $eksekutif . "<br>" . "Jumlah Tiket Ekonomi = " . $ekonomi . "<br>"
+                                    . "Total Tiket = " . $total_tiket;
+                            } else if ($eksekutif >= 20) {
+                                $keuntungan_eksekutif = 10;
+                                $total = $keuntungan_eksekutif + $keuntungan_ekonomi + $keuntungan_vip;
+                                $total_tiket = $vip + $eksekutif + $ekonomi;
+                                echo "Keuntungan VIP = " . $keuntungan_vip . "%" . "<br>" . "Keuntungan Eksekutif = " . $keuntungan_eksekutif . "%" . "<br>"
+                                    . "Keuntungan Ekonomi = " . $keuntungan_ekonomi . "%" . "<br>" . "Keuntungan Kesuluran = " . $total . "%" . "<br>" .
+                                    "Jumlah Tiket VIP = " . $vip . "<br>" . "Jumlah Tiket Eksekutif = " . $eksekutif . "<br>" . "Jumlah Tiket Ekonomi = " . $ekonomi . "<br>"
+                                    . "Total Tiket = " . $total_tiket;
+                            } else if ($eksekutif < 20) {
+                                $keuntungan_eksekutif = 2;
+                                $total = $keuntungan_eksekutif + $keuntungan_ekonomi + $keuntungan_vip;
+                                $total_tiket = $vip + $eksekutif + $ekonomi;
+                                echo "Keuntungan VIP = " . $keuntungan_vip . "%" . "<br>" . "Keuntungan Eksekutif = " . $keuntungan_eksekutif . "%" . "<br>"
+                                    . "Keuntungan Ekonomi = " . $keuntungan_ekonomi . "%" . "<br>" . "Keuntungan Kesuluran = " . $total . "%" . "<br>" .
+                                    "Jumlah Tiket VIP = " . $vip . "<br>" . "Jumlah Tiket Eksekutif = " . $eksekutif . "<br>" . "Jumlah Tiket Ekonomi = " . $ekonomi . "<br>"
+                                    . "Total Tiket = " . $total_tiket;
+                            }
+                        } else if ($vip >= 20) {
+                            $keuntungan_vip = 15;
+                            $keuntungan_ekonomi = 7;
+                            if ($eksekutif >= 40) {
+                                $keuntungan_eksekutif = 20;
+                                $total = $keuntungan_eksekutif + $keuntungan_ekonomi + $keuntungan_vip;
+                                $total_tiket = $vip + $eksekutif + $ekonomi;
+                                echo "Keuntungan VIP = " . $keuntungan_vip . "%" . "<br>" . "Keuntungan Eksekutif = " . $keuntungan_eksekutif . "%" . "<br>"
+                                    . "Keuntungan Ekonomi = " . $keuntungan_ekonomi . "%" . "<br>" . "Keuntungan Kesuluran = " . $total . "%" . "<br>" .
+                                    "Jumlah Tiket VIP = " . $vip . "<br>" . "Jumlah Tiket Eksekutif = " . $eksekutif . "<br>" . "Jumlah Tiket Ekonomi = " . $ekonomi . "<br>"
+                                    . "Total Tiket = " . $total_tiket;
+                            } else if ($eksekutif >= 20) {
+                                $keuntungan_eksekutif = 10;
+                                $total = $keuntungan_eksekutif + $keuntungan_ekonomi + $keuntungan_vip;
+                                $total_tiket = $vip + $eksekutif + $ekonomi;
+                                echo "Keuntungan VIP = " . $keuntungan_vip . "%" . "<br>" . "Keuntungan Eksekutif = " . $keuntungan_eksekutif . "%" . "<br>"
+                                    . "Keuntungan Ekonomi = " . $keuntungan_ekonomi . "%" . "<br>" . "Keuntungan Kesuluran = " . $total . "%" . "<br>" .
+                                    "Jumlah Tiket VIP = " . $vip . "<br>" . "Jumlah Tiket Eksekutif = " . $eksekutif . "<br>" . "Jumlah Tiket Ekonomi = " . $ekonomi . "<br>"
+                                    . "Total Tiket = " . $total_tiket;
+                            } else if ($eksekutif < 20) {
+                                $keuntungan_eksekutif = 2;
+                                $total = $keuntungan_eksekutif + $keuntungan_ekonomi + $keuntungan_vip;
+                                $total_tiket = $vip + $eksekutif + $ekonomi;
+                                echo "Keuntungan VIP = " . $keuntungan_vip . "%" . "<br>" . "Keuntungan Eksekutif = " . $keuntungan_eksekutif . "%" . "<br>"
+                                    . "Keuntungan Ekonomi = " . $keuntungan_ekonomi . "%" . "<br>" . "Keuntungan Kesuluran = " . $total . "%" . "<br>" .
+                                    "Jumlah Tiket VIP = " . $vip . "<br>" . "Jumlah Tiket Eksekutif = " . $eksekutif . "<br>" . "Jumlah Tiket Ekonomi = " . $ekonomi . "<br>"
+                                    . "Total Tiket = " . $total_tiket;
+                            }
+                        } else if ($vip < 20) {
+                            $keuntungan_vip = 5;
+                            $keuntungan_ekonomi = 7;
+                            if ($eksekutif >= 40) {
+                                $keuntungan_eksekutif = 20;
+                                $total = $keuntungan_eksekutif + $keuntungan_ekonomi + $keuntungan_vip;
+                                $total_tiket = $vip + $eksekutif + $ekonomi;
+                                echo "Keuntungan VIP = " . $keuntungan_vip . "%" . "<br>" . "Keuntungan Eksekutif = " . $keuntungan_eksekutif . "%" . "<br>"
+                                    . "Keuntungan Ekonomi = " . $keuntungan_ekonomi . "%" . "<br>" . "Keuntungan Kesuluran = " . $total . "%" . "<br>" .
+                                    "Jumlah Tiket VIP = " . $vip . "<br>" . "Jumlah Tiket Eksekutif = " . $eksekutif . "<br>" . "Jumlah Tiket Ekonomi = " . $ekonomi . "<br>"
+                                    . "Total Tiket = " . $total_tiket;
+                            } else if ($eksekutif >= 20) {
+                                $keuntungan_eksekutif = 10;
+                                $total = $keuntungan_eksekutif + $keuntungan_ekonomi + $keuntungan_vip;
+                                $total_tiket = $vip + $eksekutif + $ekonomi;
+                                echo "Keuntungan VIP = " . $keuntungan_vip . "%" . "<br>" . "Keuntungan Eksekutif = " . $keuntungan_eksekutif . "%" . "<br>"
+                                    . "Keuntungan Ekonomi = " . $keuntungan_ekonomi . "%" . "<br>" . "Keuntungan Kesuluran = " . $total . "%" . "<br>" .
+                                    "Jumlah Tiket VIP = " . $vip . "<br>" . "Jumlah Tiket Eksekutif = " . $eksekutif . "<br>" . "Jumlah Tiket Ekonomi = " . $ekonomi . "<br>"
+                                    . "Total Tiket = " . $total_tiket;
+                            } else if ($eksekutif < 20) {
+                                $keuntungan_eksekutif = 2;
+                                $total = $keuntungan_eksekutif + $keuntungan_ekonomi + $keuntungan_vip;
+                                $total_tiket = $vip + $eksekutif + $ekonomi;
+                                echo "Keuntungan VIP = " . $keuntungan_vip . "%" . "<br>" . "Keuntungan Eksekutif = " . $keuntungan_eksekutif . "%" . "<br>"
+                                    . "Keuntungan Ekonomi = " . $keuntungan_ekonomi . "%" . "<br>" . "Keuntungan Kesuluran = " . $total . "%" . "<br>" .
+                                    "Jumlah Tiket VIP = " . $vip . "<br>" . "Jumlah Tiket Eksekutif = " . $eksekutif . "<br>" . "Jumlah Tiket Ekonomi = " . $ekonomi . "<br>"
+                                    . "Total Tiket = " . $total_tiket;
+                            }
+                        }
+                    } else {
+                        echo "Tiket Tidak Boleh Lebih Dari 50";
+                    }
+                } else {
+                    echo "Tiket Tidak Boleh Lebih Dari 50";
+                }
+            } else {
+                echo "Tiket Tidak Boleh Lebih Dari 50";
+            }
+        }
 
-    <?php
-     $nama;
-     $kehadiran;
-     $mtk;
-     $pai;
-     $dpk;
-     $indo;
-     $ing;
-     
-     $data_siswa=array(
-     '$kehadiran',
-     '$mtk',
-     '$indo',
-     '$ing',
-     '$pai',
-     '$dpk'
-     );
-
-     $data=array($data_siswa);
-    
-     count($data);
-     
-     if (isset($_POST['submit'])) {
-        $no_pegawai = $_POST ['no_pegawai'];
-        $length = strlen($no_pegawai);
-
-     if(count($data) == 15){
-
-
-
-
-     }
-    }
-    ?>
-    </table>
-</form>
+        ?>
+    </form>
 </body>
+
 </html>
