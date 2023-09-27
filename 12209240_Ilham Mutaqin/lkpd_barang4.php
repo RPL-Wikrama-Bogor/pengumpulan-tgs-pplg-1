c0d96082653eedd833e53c80f508ef6b518b71dc
Ilhammtqn/CV-Ilham <muhammadIlhamMutaqin@smkwikrama.sch.id>
2023-09-27 18:16:24 +0700

    PHPMVC

--- 12209240_Ilham Mutaqin/lkpd_barang4.php
+++
@@ -1,7 +0,0 @@
-<?php 
-foreach ($menus as $key => $value) { 
-    if ($value['tipe'] == 'minuman') {
-         echo "<option value="" , $key "'>" , $value['menu'] , "</option>"; 
-        }
-    }
-?>
