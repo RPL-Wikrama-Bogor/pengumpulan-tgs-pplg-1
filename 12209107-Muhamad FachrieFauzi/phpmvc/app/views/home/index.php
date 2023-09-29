8b61b5860fb60c25b1ca5286b44221dedb62b2dd
fachrieubed <m.fachriefauzi@smkwikrama.sch.id>
2023-09-29 08:30:42 +0700

    tugas mvc

---
+++ 12209107-Muhamad FachrieFauzi/phpmvc/app/views/home/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="jumbotron py-4 px-5">
+        <h1 class="display-4">Selamat Datang !</h1>
+        <hr class="my-4">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel dicta cumque sint esse blanditiis 
+        corporis eius enim delectus, quam saepe dolorum deleniti. Et, exercitationem dignissimos.</p>
+    </div>
+</div>
