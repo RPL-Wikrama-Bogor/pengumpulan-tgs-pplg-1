3520640d18bae0c3429cf2433d27876b02079d2d
asty777 <nikadek010933@gmail.com>
2023-10-09 14:32:22 +0700

    study case mvc

---
+++ 12209318-NikadekBelindaAsty/mvc_pinjaman/app/config/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASE_URL', 'http://localhost/pengumpulan-tgs-pplg-1/12209318-NikadekBelindaAsty/mvc_pinjaman/public');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'db_pinjaman');
+?>
---
+++ 12209318-NikadekBelindaAsty/mvc_pinjaman/app/views/home/index.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class=" py-4 px-6">
+        <h1 class="display-4">Selamat datang!</h1>
+        <hr class="my-4">  
+    </div> 
+</div> 
+<div class="container">
+     <h4 class=" fs-2 display-4 py-4 px-3">Inventaris Alat Pembelajaran</h4>
+    <ol class="list-group-numbered">
+        <h4 class="fs-4">Peraturan Peminjaman</h4>
+        <li class="list-group-item">Menemui petugas inventaris ketika akan meminjam dan mengembalikan barang</li>
+        <li class="list-group-item">Lama waktu peminjaman hanya 2 hari</li>
+        <li class="list-group-item">Menyerahkan kartu identitas ketika melakukan peminjam sebagai jaminan</li>
+    </ol>
+</div>     
