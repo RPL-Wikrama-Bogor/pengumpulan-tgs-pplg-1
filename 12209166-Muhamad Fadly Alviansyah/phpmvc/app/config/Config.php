232023d61bc26c7f56e85aac8fdd74bb3e73e9b7
FadlyAlviansyah <muhamadfadlyalviansyah@smkwikrama.sch.id>
2023-10-30 15:23:36 +0700

    Vue JS Senin, 30 Oktober 2023

--- 12209166-Muhamad Fadly Alviansyah/phpmvc/app/config/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('BASE_URL', 'http://localhost/pengumpulan-tgs-pplg-1/12209166-Muhamad%20Fadly%20Alviansyah/phpmvc/public');
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'db_oop_mvc');
--- 12209166-Muhamad Fadly Alviansyah/phpmvc/app/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'core/App.php';
-require_once 'core/Controller.php';
-require_once 'config/Config.php';
-require_once 'core/Database.php';
-
-$app = new App();
