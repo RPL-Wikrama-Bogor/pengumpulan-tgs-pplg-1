0e5f76f9f908dc2ded64c4c628fb4d07c71c0919
Vincent <59721511+vinsrep@users.noreply.github.com>
2024-01-05 07:36:40 +0700

    Delete 12209318-NikadekBelindaAsty/vue-jsfull  directory
    Typo; backup already made

--- 12209318-NikadekBelindaAsty/vue-jsfull /vue-components/vue-components/src/components/mycomponents/Button.vue
+++
@@ -1,5 +0,0 @@
-<template>
-   <div>
-    <button v-bind="$attrs" class="button-service-global">Components global</button>
-   </div>
-</template>
--- 12209318-NikadekBelindaAsty/vue-jsfull /vue-router/vue-routes/src/pages/About.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    About
-</template>
